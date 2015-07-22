db9c71e412f1c76f5dbb3f833c14971776af02bd
missweb <99maribel99@gmail.com>
2015-07-23 00:00:14 +0200

    Validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -21,13 +21,13 @@ exports.index = function(req, res) {
     where = {where: ["pregunta like ?", search.replace(/(^|\s|$)/g, "%")], order: 'pregunta'};
   }
   models.Quiz.findAll(where).then(function(quizes) {
-    res.render('quizes/index.ejs', {quizes: quizes, query: search});
+    res.render('quizes/index.ejs', {quizes: quizes, query: search, errors: []});
   }).catch(function(error) { next(error);} );
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -36,7 +36,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -45,15 +45,25 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
-// POST /quizes/create
+//POST /quizes/create
 exports.create = function(req, res) {
-  var quiz = models.Quiz.build( req.body.quiz );
-
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+  var quiz = models.Quiz.build(req.body.quiz);
+  quiz
+  .validate()
+  .then(function(err) {
+    if(err) {
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    } else {
+      quiz
+      .save({
+        fields: ["pregunta", "respuesta"]
+      })
+      .then(function() {
+        res.redirect('/quizes');
+      })
+    }
+  })
 };
