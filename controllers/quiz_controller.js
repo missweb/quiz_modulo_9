17820816b9634f5d8bacaac8b744d3a474f1ee85
missweb <99maribel99@gmail.com>
2015-07-24 22:46:56 +0200

    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -65,7 +65,7 @@ exports.create = function(req, res) {
         res.redirect('/quizes');
       })
     }
-  })
+  }).catch(function(error){next(error)});
 };
 
 // GET /quizes/:id/edit
@@ -92,5 +92,12 @@ exports.update = function(req, res) {
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
-  );
+  ).catch(function(error){next(error)});
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
 };
